1eaceab385f3779d9ab0b85a5d67a6a33d9de14c
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2024-11-30 20:25:25 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -60,7 +60,7 @@ export default defineConfig({
     '/how-to-create-android-universal-recycler-view-adapter-with-mvvm-and-data-binding': 'https://yogeshpaliyal.hashnode.dev/how-to-create-android-universal-recycler-view-adapter-with-mvvm-and-data-binding',
     '/android-material-design-tabs-tab-layout-with-swipe-or-basic':'https://yogeshpaliyal.hashnode.dev/android-material-design-tabs-tab-layout-with-swipe-or-basic',
     '/android-bottom-navigation-with-cut-out-design':'https://yogeshpaliyal.hashnode.dev/android-bottom-navigation-with-cut-out-design',
-    '/firebase-messaging-with-image-in-android-or-android-kotlin-push-notification':'https://yogeshpaliyal.hashnode.dev/firebase-messaging-with-image-in-android-or-android-kotlin-push-notification',
+    '/firebase-messaging-with-image-in-android-or-android-kotlin-push-notification':'/posts/firebase-messaging-with-image-in-android-or-android-kotlin-push-notification',
     '/keypass-password-manager':'https://yogeshpaliyal.hashnode.dev/keypass-password-manager',
     '/keypass-privacy-policy':'https://yogeshpaliyal.hashnode.dev/keypass-privacy-policy',
     '/archive': '/archives?utm_source=yogeshpaliyal-old&utm_medium=redirect_url&utm_campaign=redirect_url',
