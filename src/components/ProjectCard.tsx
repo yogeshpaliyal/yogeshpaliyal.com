40b4a865b3a17a5e076c760a7c53d7a7394c8680
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2024-12-25 22:11:50 +0530

    feat: show stars and forks in projects and sort by priority

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,42 @@
+import { slugifyStr } from "@utils/slugify";
+import Datetime from "./Datetime";
+import type { CollectionEntry } from "astro:content";
+import ProjectStats from "./ProjectStats";
+
+export interface Props {
+  href?: string;
+  frontmatter: CollectionEntry<"projects">["data"];
+  secHeading?: boolean;
+  hideDateTime?: boolean;
+}
+
+export default function ProjectCard({
+  href,
+  frontmatter,
+  secHeading = true,
+  hideDateTime = false,
+}: Props) {
+  const { title, pubDatetime, modDatetime, description, repoName, username } = frontmatter;
+
+  const headerProps = {
+    style: { viewTransitionName: slugifyStr(title) },
+    className: "text-lg font-medium decoration-dashed hover:underline",
+  };
+
+  return (
+    <li className="my-6">
+      <a
+        href={href}
+        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+      >
+        {secHeading ? (
+          <h2 {...headerProps}>{title}</h2>
+        ) : (
+          <h3 {...headerProps}>{title}</h3>
+        )}
+      </a>
+      <ProjectStats repoName={repoName} username={username} />
+      <p>{description}</p>
+    </li>
+  );
+}
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { SITE } from "@config";
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
@@ -35,36 +35,39 @@ const blog = defineCollection({
 });
 
 const projects = defineCollection({
-  type: "content_layer",
-  loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
+  loader: file("./src/content/projects/"),
   schema: ({ image }) =>
-    z.object({
-      contentType: z.string().default("projects"),
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
-      title: z.string(),
-      url: z.string(),
-      featured: z.boolean().optional(),
-      draft: z.literal("unlisted").or(z.boolean()).optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
-        })
-        .or(z.string())
-        .optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional(),
-      editPost: z
-        .object({
-          disabled: z.boolean().optional(),
-          url: z.string().optional(),
-          text: z.string().optional(),
-          appendFilePath: z.boolean().optional(),
-        })
-        .optional(),
-    }),
+      z.object({
+        contentType: z.string().default("projects"),
+        author: z.string().default(SITE.author),
+        pubDatetime: z.date().optional().nullable(),
+        modDatetime: z.date().optional().nullable(),
+        title: z.string(),
+        url: z.string(),
+        featured: z.boolean().optional(),
+        priority: z.number().default(999).optional(),
+        projectType: z.literal("github").default("github"),
+        username: z.string().optional(),
+        repoName: z.string().optional(),
+        draft: z.literal("unlisted").or(z.boolean()).optional(),
+        tags: z.array(z.string()).default(["others"]),
+        ogImage: image()
+          .refine(img => img.width >= 1200 && img.height >= 630, {
+            message: "OpenGraph image must be at least 1200 X 630 pixels!",
+          })
+          .or(z.string())
+          .optional(),
+        description: z.string(),
+        canonicalURL: z.string().optional(),
+        editPost: z
+          .object({
+            disabled: z.boolean().optional(),
+            url: z.string().optional(),
+            text: z.string().optional(),
+            appendFilePath: z.boolean().optional(),
+          })
+          .optional(),
+      }),
 });
 
 const quickTips = defineCollection({
